1db22bbab47db7d81bc3e5a818d12d266b78ddb3
selfservit <chak@selfservit.com>
2016-11-07 10:46:55 +0530

    Update mInterface.java

--- src/android/mInterface.java
+++ src/android/mInterface.java
@@ -1,26 +1,14 @@
 package com.selfservit.util;
 
-import org.apache.cordova.CordovaInterface;
-import android.util.Log;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.provider.Settings;
-import android.widget.Toast;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.apache.cordova.*;
 
 public class mInterface extends CordovaPlugin{
-	public mInterface(){
-		//constructor
-	}
-	
 	@Override
 	public boolean execute(final String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 		if(action.equals("StartService")){
@@ -30,53 +18,21 @@ public class mInterface extends CordovaPlugin{
 				callbackContext.success("Success");
 			}
 		}else if(action.equals("GetLocation")){
-			LocationManager locationManager = (LocationManager) cordova.getActivity().getApplicationContext().getSystemService(cordova.getActivity().getApplicationContext().LOCATION_SERVICE);
-			LocationListener locationListener = new LocationListener() {
-				@Override
-				public void onLocationChanged(Location location) {
-
-				}
-
-				@Override
-				public void onStatusChanged(String provider, int status, Bundle extras) {
-
-				}
-
-				@Override
-				public void onProviderEnabled(String provider) {
-
-				}
-
-				@Override
-				public void onProviderDisabled(String provider) {
-
-				}
-			};
-			locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, locationListener);
-
-	       		double latitude = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER).getLatitude();
-    	   	        double longitude=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER).getLongitude();
-			callbackContext.success("{\"lat\":"+"\""+latitude+"\""+",\"lon\":"+"\""+longitude+"\"}");
+			callbackContext.success(new mInerfaceUtil().getLocation(cordova.getActivity().getApplicationContext()));
 		}else if(action.equals("CheckLocation")){
-			boolean location_enabled=false;
-			LocationManager locationManager = (LocationManager) cordova.getActivity().getApplicationContext().getSystemService(cordova.getActivity().getApplicationContext().LOCATION_SERVICE);
-			location_enabled=locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-			if(location_enabled) {
-				callbackContext.success("true");
-			} else {
-				callbackContext.success("false");
-			}
-			
+			callbackContext.success(new mInerfaceUtil().checkLocation(cordova.getActivity().getApplicationContext()));
 		}
 		return true;
-	}	
+	}
+
 	private boolean isServiceRunning(Class<?> serviceClass) {
-        ActivityManager manager = (ActivityManager)this.cordova.getActivity().getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (serviceClass.getName().equals(service.service.getClassName())) {
-                return true;
-            }
-        }
-        return false;
-    }
+		ActivityManager manager = (ActivityManager)cordova.getActivity().getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
+		for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+			if (serviceClass.getName().equals(service.service.getClassName())) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
+
