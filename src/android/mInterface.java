6237f0bf1de2f3bd96285541f800b0bc0365fd65
selfservit <chak@selfservit.com>
2016-11-08 13:32:52 +0530

    Update mInterface.java

--- src/android/mInterface.java
+++ src/android/mInterface.java
@@ -1,14 +1,18 @@
 package com.selfservit.util;
 
+import android.annotation.TargetApi;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.apache.cordova.*;
 
 public class mInterface extends CordovaPlugin{
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
 	@Override
 	public boolean execute(final String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 		if(action.equals("StartService")){
@@ -21,6 +25,16 @@ public class mInterface extends CordovaPlugin{
 			callbackContext.success(new mInterfaceUtil().getLocation(cordova.getActivity().getApplicationContext()));
 		}else if(action.equals("CheckLocation")){
 			callbackContext.success(new mInterfaceUtil().checkLocation(cordova.getActivity().getApplicationContext()));
+		}else if(action.equals("CheckDateTimeAccuracy")){
+			try {
+				if (Settings.Global.getInt(this.cordova.getActivity().getContentResolver(), Settings.Global.AUTO_TIME) == 1) {
+					callbackContext.success("true");
+				}else{
+					callbackContext.success("false");
+				}
+			} catch (Settings.SettingNotFoundException e) {
+				e.printStackTrace();
+			}
 		}
 		return true;
 	}
