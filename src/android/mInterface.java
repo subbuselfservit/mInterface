9c3baafaea568ac9e685099e0e35a9080078b397
selfservit <chak@selfservit.com>
2016-11-10 19:14:19 +0530

    Update mInterface.java

--- src/android/mInterface.java
+++ src/android/mInterface.java
@@ -1,6 +1,7 @@
 package com.selfservit.util;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +13,8 @@ import org.json.JSONException;
 import org.apache.cordova.*;
 
 public class mInterface extends CordovaPlugin{
+	private static final int PICK_FILE_REQUEST= 1;
+	CallbackContext callback;
 	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
 	@Override
 	public boolean execute(final String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
@@ -35,8 +38,43 @@ public class mInterface extends CordovaPlugin{
 			} catch (Settings.SettingNotFoundException e) {
 				e.printStackTrace();
 			}
+		}else if(action.equals("FileChooser")){
+			chooseFile(callbackContext);
+			return true;
 		}
 		return true;
+	}
+	public void chooseFile(CallbackContext callbackContext) {
+		Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+		intent.setType("*/*");
+		intent.addCategory(Intent.CATEGORY_OPENABLE);
+		intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+
+		Intent chooser = Intent.createChooser(intent, "Select File");
+		cordova.startActivityForResult(this, chooser, PICK_FILE_REQUEST);
+
+		PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+		pluginResult.setKeepCallback(true);
+		callback = callbackContext;
+		callbackContext.sendPluginResult(pluginResult);
+	}
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == PICK_FILE_REQUEST && callback != null) {
+			if (resultCode == Activity.RESULT_OK) {
+				String filePath = data.getData().getPath();
+				if (filePath != null) {
+					callback.success(filePath);
+				} else {
+					callback.error("File uri was null");
+				}
+			} else if (resultCode == Activity.RESULT_CANCELED) {
+				// TODO NO_RESULT or error callback?
+				callback.error("failure");
+			} else {
+				callback.error(resultCode);
+			}
+		}
 	}
 
 	private boolean isServiceRunning(Class<?> serviceClass) {
