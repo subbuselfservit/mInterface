a9475b6c73593a3f8877995fc2c9bb60fae6855a
selfservit <chak@selfservit.com>
2016-11-17 15:24:34 +0530

    Update mInterface.java

--- src/android/mInterface.java
+++ src/android/mInterface.java
@@ -1,19 +1,16 @@
 package com.selfservit.util;
 
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.provider.MediaStore;
-import android.provider.Settings;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.apache.cordova.*;
+import org.apache.cordova. * ;
 import org.json.JSONObject;
 
 import java.io.File;
@@ -22,75 +19,71 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class mInterface extends CordovaPlugin{
-	private static final int PICK_FILE_REQUEST= 1;
+public class mInterface extends CordovaPlugin {
+	private static final int PICK_FILE_REQUEST = 1;
 	CallbackContext callback;
-	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
-	@Override
-	public boolean execute(final String action, JSONArray args,  CallbackContext callbackContext) throws JSONException {
-		if(action.equals("StartService")){
-			if(!isServiceRunning(mInterfaceService.class)) {
+	@ Override
+	public boolean execute(final String action, JSONArray args, CallbackContext callbackContext)throws JSONException {
+		if (action.equals("StartService")) {
+			if (!isServiceRunning(mInterfaceService.class)) {
 				Intent serviceIntent = new Intent(cordova.getActivity().getApplicationContext(), mInterfaceService.class);
 				cordova.getActivity().startService(serviceIntent);
 				callbackContext.success("Success");
 			}
-		}else if(action.equals("GetLocation")){
+		} else if (action.equals("GetLocation")) {
 			callbackContext.success(new mInterfaceUtil().getLocation(cordova.getActivity().getApplicationContext()));
-		}else if(action.equals("CheckLocation")){
+		} else if (action.equals("CheckLocation")) {
 			callbackContext.success(new mInterfaceUtil().checkLocation(cordova.getActivity().getApplicationContext()));
-		}else if(action.equals("CheckDateTimeAccuracy")){
-			try {
-				if (Settings.Global.getInt(this.cordova.getActivity().getContentResolver(), Settings.Global.AUTO_TIME) == 1) {
-					callbackContext.success("true");
-				}else{
-					callbackContext.success("false");
-				}
-			} catch (Settings.SettingNotFoundException e) {
-				e.printStackTrace();
-			}
-		}else if(action.equals("FileChooser")){
+		} else if (action.equals("FileChooser")) {
 			chooseFile(callbackContext);
 			return true;
-		}else if(action.equals("CopyFile")){
+		} else if (action.equals("CopyFile")) {
 			JSONObject fileObj;
-			String srcPath,srcFile,desPath,desFile;
-			File srcPathObj,desPathObj,sourceFile,destinationFile;
-				 fileObj = args.getJSONObject(0);
-				 srcPath = fileObj.optString("srcPath").toString();
-				 srcFile = fileObj.optString("srcFile").toString();
-				 desPath = fileObj.optString("desPath").toString();
-				 desFile = fileObj.optString("desFile").toString();
-				 srcPathObj = new File(srcPath);
-				 desPathObj = new File(desPath);
-				 sourceFile = new File(srcPath + "/" +srcFile);
-				 destinationFile = new File (desPath + "/" +desFile);
-					try {
-						if(!srcPathObj.exists()){
-							callbackContext.success("failure");
-						}
-						if(!desPathObj.exists()){
-							desPathObj.mkdir();
-						}
-						if(sourceFile.exists()){
-							InputStream in = new FileInputStream(sourceFile);
-							OutputStream out = new FileOutputStream(destinationFile);
-							byte[] buf = new byte[1024];
-							int len;
-							while ((len = in.read(buf)) > 0) {
-								out.write(buf, 0, len);
-							}
-							in.close();
-							out.close();
-							callbackContext.success("success");
-						}else{
-							callbackContext.success("failure");
-						}
-					} catch (NullPointerException e) {
-						e.printStackTrace();
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+			String srcPath,
+					srcFile,
+					desPath,
+					desFile;
+			File srcPathObj,
+					desPathObj,
+					sourceFile,
+					destinationFile;
+			fileObj = args.getJSONObject(0);
+			srcPath = fileObj.optString("srcPath").toString();
+			srcFile = fileObj.optString("srcFile").toString();
+			desPath = fileObj.optString("desPath").toString();
+			desFile = fileObj.optString("desFile").toString();
+			srcPathObj = new File(srcPath);
+			desPathObj = new File(desPath);
+			sourceFile = new File(srcPath + "/" + srcFile);
+			destinationFile = new File(desPath + "/" + desFile);
+			try {
+				if (!srcPathObj.exists()) {
+					callbackContext.success("failure");
 				}
+				if (!desPathObj.exists()) {
+					desPathObj.mkdirs();
+				}
+				if (sourceFile.exists()) {
+					InputStream in = new FileInputStream(sourceFile);
+					OutputStream out = new FileOutputStream(destinationFile);
+					byte[]buf = new byte[1024];
+					int len;
+					while ((len = in.read(buf)) > 0) {
+						out.write(buf, 0, len);
+					}
+					in.close();
+					out.close();
+					callbackContext.success("success");
+				} else {
+					callbackContext.success("failure");
+				}
+			} catch (NullPointerException e) {
+				e.printStackTrace();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 		return true;
 	}
 	public void chooseFile(CallbackContext callbackContext) {
@@ -106,43 +99,51 @@ public class mInterface extends CordovaPlugin{
 		callback = callbackContext;
 		callbackContext.sendPluginResult(pluginResult);
 	}
-	@Override
+	@ Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == PICK_FILE_REQUEST && callback != null) {
 			if (resultCode == Activity.RESULT_OK) {
-				String fileName = null,getfilePath,filePath = null,fileExtension = null,fileType,fileSize=null;
+				String fileName = null,
+						getfilePath,
+						filePath = null,
+						fileExtension = null,
+						fileType,
+						fileSize = null;
 				long getSize;
 				Uri uri = data.getData();
-				fileType =this.cordova.getActivity().getContentResolver().getType(uri);
+				fileType = this.cordova.getActivity().getContentResolver().getType(uri);
 				File getFileName;
-				if(fileType != null) {
-					String[] projection = {MediaStore.Images.Media.DATA,MediaStore.Images.Media.SIZE};
+				if (fileType != null) {
+					String[]projection = {
+							MediaStore.Images.Media.DATA,
+							MediaStore.Images.Media.SIZE
+					};
 					Cursor cursor = this.cordova.getActivity().getContentResolver().query(uri, projection, null, null, null);
 					if (cursor.moveToFirst()) {
 						int index1 = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.SIZE);
 						int actual_image_column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
 						getfilePath = cursor.getString(actual_image_column_index);
-						filePath =getfilePath.substring(0,getfilePath.lastIndexOf(File.separator));
-						fileName = getfilePath.substring(getfilePath.lastIndexOf("/")+1);
-						fileExtension = "."+fileName.substring(fileName.lastIndexOf(".")+1);
+						filePath = getfilePath.substring(0, getfilePath.lastIndexOf(File.separator));
+						fileName = getfilePath.substring(getfilePath.lastIndexOf("/") + 1);
+						fileExtension = "." + fileName.substring(fileName.lastIndexOf(".") + 1);
 						fileSize = cursor.getString(index1);
 					}
-				}else{
-						getfilePath =data.getData().getPath();
-						filePath = getfilePath.substring(0, getfilePath.lastIndexOf(File.separator));
-						getFileName = new File(getfilePath);
-						fileName =getFileName.getName();
-						fileExtension ="."+ getfilePath.substring(getfilePath.lastIndexOf(".") + 1);
-						getSize =getFileName.length();
-						fileSize = String.valueOf(getSize);
+				} else {
+					getfilePath = data.getData().getPath();
+					filePath = getfilePath.substring(0, getfilePath.lastIndexOf(File.separator));
+					getFileName = new File(getfilePath);
+					fileName = getFileName.getName();
+					fileExtension = "." + getfilePath.substring(getfilePath.lastIndexOf(".") + 1);
+					getSize = getFileName.length();
+					fileSize = String.valueOf(getSize);
 
 				}
 				JSONObject jsonObject = new JSONObject();
 				try {
-					jsonObject.put("filePath",filePath);
-					jsonObject.put("fileName",fileName);
-					jsonObject.put("fileSize",fileSize);
-					jsonObject.put("fileExtension",fileExtension);
+					jsonObject.put("filePath", filePath);
+					jsonObject.put("fileName", fileName);
+					jsonObject.put("fileSize", fileSize);
+					jsonObject.put("fileExtension", fileExtension);
 				} catch (JSONException e) {
 					e.printStackTrace();
 				}
@@ -159,9 +160,9 @@ public class mInterface extends CordovaPlugin{
 			}
 		}
 	}
-	private boolean isServiceRunning(Class<?> serviceClass) {
+	private boolean isServiceRunning(Class <  ?  > serviceClass) {
 		ActivityManager manager = (ActivityManager)cordova.getActivity().getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
-		for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+		for (ActivityManager.RunningServiceInfo service: manager.getRunningServices(Integer.MAX_VALUE)) {
 			if (serviceClass.getName().equals(service.service.getClassName())) {
 				return true;
 			}
@@ -169,4 +170,5 @@ public class mInterface extends CordovaPlugin{
 		return false;
 	}
 }
+
 
