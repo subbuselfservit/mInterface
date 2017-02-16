6bd1fe0494f934c889933ce33c889c36d5bb64bd
selfservit <chak@selfservit.com>
2017-02-16 12:53:23 +0530

    Add files via upload

---
+++ src/ios/Utils.m
@@ -0,0 +1,81 @@
+//
+//  Utils.m
+//  MService
+//
+//  Created by jayaraj on 05/12/16.
+//  Copyright © 2016 sample. All rights reserved.
+//
+
+#import "Utils.h"
+
+@implementation Utils
+
+- (void)UpdateLocation {
+    // set LocationManager properties
+    _locationManager = [[CLLocationManager alloc]init];
+    _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    _locationManager.delegate = self;
+    if ([_locationManager respondsToSelector:@selector(requestAlwaysAuthorization)]) {
+        [_locationManager requestAlwaysAuthorization];
+    }
+    
+    // Allow background Update
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 9) {
+        _locationManager.allowsBackgroundLocationUpdates = YES;
+    }
+    _locationManager.pausesLocationUpdatesAutomatically = NO;
+    [_locationManager startUpdatingLocation];
+}
+
+#pragma mark - MapViewDelegate
+- (void) locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray<CLLocation *> *)locations{
+    //    NSLog(@"%@",[locations objectAtIndex:0]);
+}
+
+// Error while updating location
+- (void) locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    NSLog(@"%@",error);
+}
+
+- (id)init {
+    self = [super init];
+    
+    if(self) {
+        self.locationManager = [CLLocationManager new];
+        [self.locationManager setDelegate:self];
+        [self.locationManager setDistanceFilter:kCLDistanceFilterNone];
+        [self.locationManager setHeadingFilter:kCLHeadingFilterNone];
+        
+        // Allow background Update
+        if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 9) {
+            _locationManager.allowsBackgroundLocationUpdates = YES;
+        }
+        [self.locationManager startUpdatingLocation];
+        //do any more customization to your location manager
+    }
+    
+    return self;
+}
+- (void)locationManager:(CLLocationManager *)manager didUpdateToLocation:(CLLocation *)newLocation fromLocation:(CLLocation *)oldLocation {
+    //handle your location updates here
+    // NSLog(@"%@", newLocation);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateHeading:(CLHeading *)newHeading {
+    //handle your heading updates here- I would suggest only handling the nth update, because they
+    //come in fast and furious and it takes a lot of processing power to handle all of them
+}
+
++ (Utils*)sharedSingleton {
+    static Utils* sharedSingleton;
+    if(!sharedSingleton) {
+        static dispatch_once_t onceToken;
+        dispatch_once(&onceToken, ^{
+            sharedSingleton = [Utils new];
+        });
+    }
+    
+    return sharedSingleton;
+}
+
+@end
