2ce5b2c0eaab5421dab55c4b013a83d579158fac
selfservit <chak@selfservit.com>
2017-03-04 11:05:04 +0530

    Update HWPHello.m

--- src/ios/HWPHello.m
+++ src/ios/HWPHello.m
@@ -152,24 +152,43 @@
         double lat = [Utils sharedSingleton].locationManager.location.coordinate.latitude;
         double lngt = [Utils sharedSingleton].locationManager.location.coordinate.longitude;
         NSString *locationString = [NSString stringWithFormat:@"{\"lat\":\"%f\",\"lon\":\"%f\"}", lat, lngt];
-        NSString *domain_name = @"203.124.121.207";
-        NSString *port_no = @"81";
-        NSString *protocol_type = @"http:";
+       // NSString *domain_name = @"203.124.121.207";
+       // NSString *port_no = @"81";
+       // NSString *protocol_type = @"http:";
         NSString *clientID = @"dev";
         NSString *countryCode =@"in";
         NSString *deviceID = @"43D09A1E-C140-4240-B715-41A27EE941CD";
         NSString *locationData = @"12.4816286,78.965844,20170222151025";
                 
+		NSString *docdir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+		NSString *access_pack_path = [NSString stringWithFormat:@"%@%@%@/%@/%@",docdir,@"/mservice/client_functional_access_package/",clientID,countryCode,@"client_functional_access.xml"];
+		
         //Send Data to server
-        NSString *baseURL = [NSString stringWithFormat:@"%@//%@:%@/common/components/GeoLocation/update_device_location_offline.aspx",protocol_type,domain_name, port_no];
-        NSString *content = [NSString stringWithFormat:@"<location_xml><client_id>%@</client_id><country_code>%@</country_code><device_id>%@</device_id><location>%@</location></location_xml>", clientID, countryCode, deviceID, locationData];
-        NSData *data = [content dataUsingEncoding:NSUTF8StringEncoding];
-        NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:baseURL]];
-        [request setValue:@"text/xml" forHTTPHeaderField:@"Content-type"];
-        [request setHTTPMethod : @"POST"];
-        [request setHTTPBody : data];
-        // generates an autoreleased NSURLConnection
-        [NSURLConnection connectionWithRequest:request delegate:self];
+       [XMLConverter convertXMLFile:access_pack_path completion:^(BOOL success, NSDictionary *dictionary, NSError *error)
+             {
+            if (success) {
+                 NSDictionary * dict = [dictionary objectForKey:@"functional_access_detail"];
+                 NSString *domain_name = [dict objectForKey:@"domain_name"];
+                 NSString *port_no = [dict objectForKey:@"port_no"];
+                 NSString *protocol_type = [dict objectForKey:@"protocol_type"];
+                
+                 //Send Data to server
+                 NSString *baseURL = [NSString stringWithFormat:@"%@//%@:%@/common/components/GeoLocation/update_device_location_offline.aspx",protocol_type,domain_name, port_no];
+                 NSString *content = [NSString stringWithFormat:@"<location_xml><client_id>%@</client_id><country_code>%@</country_code><device_id>%@</device_id><location>%@</location></location_xml>", clientID, countryCode, deviceID, locationData];
+                 NSData *data = [content dataUsingEncoding:NSUTF8StringEncoding];
+                 NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:baseURL]];
+                 [request setValue:@"text/xml" forHTTPHeaderField:@"Content-type"];
+                 [request setHTTPMethod : @"POST"];
+                 [request setHTTPBody : data];
+                
+                 if([[NSFileManager defaultManager] fileExistsAtPath:docFullPath isDirectory:false]){
+                     // Dealloc txt file
+                     [[NSData data] writeToFile:docFullPath atomically:true];
+                 }
+                 // generates an autoreleased NSURLConnection
+                 [NSURLConnection connectionWithRequest:request delegate:self];
+             }
+             }];
         //NSLog(@"%@", locationString);
         CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:locationString];
         [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
