ee5f7b4a9ca0d43f34ed122dd944c7b90b3cc65c
selfservit <chak@selfservit.com>
2017-03-06 19:10:57 +0530

    Update HWPHello.m

--- src/ios/HWPHello.m
+++ src/ios/HWPHello.m
@@ -151,7 +151,8 @@
 {
     //double lat = [Utils sharedSingleton].locationManager.location.coordinate.latitude;
     //double lngt = [Utils sharedSingleton].locationManager.location.coordinate.longitude;
-   // NSString *locationString = [NSString stringWithFormat:@"{\"lat\":\"%f\",\"lon\":\"%f\"}", lat, lngt];
+    // NSString *locationString = [NSString stringWithFormat:@"{\"lat\":\"%f\",\"lon\":\"%f\"}", lat, lngt];
+    CDVPluginResult *result = nil;
     NSString *docdir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
     NSString *user_file_path = [NSString stringWithFormat:@"%@%@",docdir,@"/mservice/user.txt"];
     NSData *user_data = [NSData dataWithContentsOfFile:user_file_path];
@@ -163,33 +164,34 @@
     NSString *locationData = @"12.12121212,13.13131313,20170305181612";
     
     NSString *access_pack_path = [NSString stringWithFormat:@"%@%@%@/%@/%@",docdir,@"/mservice/client_functional_access_package/",clientID,countryCode,@"client_functional_access.xml"];
-            //Convert XML to JSON
-            [XMLConverter convertXMLFile:access_pack_path completion:^(BOOL success, NSDictionary *dictionary, NSError *error)
-             {
-            if (success) {
-                 NSDictionary * dict = [dictionary objectForKey:@"functional_access_detail"];
-                 NSString *domain_name = [dict objectForKey:@"domain_name"];
-                 NSString *port_no = [dict objectForKey:@"port_no"];
-                 NSString *protocol_type = [dict objectForKey:@"protocol_type"];
-                
-                 //Send Data to server
-                 NSString *baseURL = [NSString stringWithFormat:@"%@//%@:%@/common/components/GeoLocation/update_device_location_offline.aspx",protocol_type,domain_name, port_no];
-                 NSString *content = [NSString stringWithFormat:@"<location_xml><client_id>%@</client_id><country_code>%@</country_code><device_id>%@</device_id><location>%@</location></location_xml>", clientID, countryCode, deviceID, locationData];
-                 NSData *data = [content dataUsingEncoding:NSUTF8StringEncoding];
-                 NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:baseURL]];
-                 [request setValue:@"text/xml" forHTTPHeaderField:@"Content-type"];
-                 [request setHTTPMethod : @"POST"];
-                 [request setHTTPBody : data];
-                
-                 /*if([[NSFileManager defaultManager] fileExistsAtPath:docFullPath isDirectory:false]){
-                     // Dealloc txt file
-                     [[NSData data] writeToFile:docFullPath atomically:true];
-                 }*/
-                 // generates an autoreleased NSURLConnection
-                 [NSURLConnection connectionWithRequest:request delegate:self];
-             }
-             }];
-    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:outData];
+    //Convert XML to JSON
+    [XMLConverter convertXMLFile:access_pack_path completion:^(BOOL success, NSDictionary *dictionary, NSError *error)
+     {
+         if (success) {
+             NSDictionary * dict = [dictionary objectForKey:@"functional_access_detail"];
+             NSString *domain_name = [dict objectForKey:@"domain_name"];
+             NSString *port_no = [dict objectForKey:@"port_no"];
+             NSString *protocol_type = [dict objectForKey:@"protocol_type"];
+             
+             //Send Data to server
+             NSString *baseURL = [NSString stringWithFormat:@"%@//%@:%@/common/components/GeoLocation/update_device_location_offline.aspx",protocol_type,domain_name, port_no];
+             NSString *content = [NSString stringWithFormat:@"<location_xml><client_id>%@</client_id><country_code>%@</country_code><device_id>%@</device_id><location>%@</location></location_xml>", clientID, countryCode, deviceID, locationData];
+             NSData *data = [content dataUsingEncoding:NSUTF8StringEncoding];
+             NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:baseURL]];
+             [request setValue:@"text/xml" forHTTPHeaderField:@"Content-type"];
+             [request setHTTPMethod : @"POST"];
+             [request setHTTPBody : data];
+             
+             /*if([[NSFileManager defaultManager] fileExistsAtPath:docFullPath isDirectory:false]){
+              // Dealloc txt file
+              [[NSData data] writeToFile:docFullPath atomically:true];
+              }*/
+             // generates an autoreleased NSURLConnection
+             [NSURLConnection connectionWithRequest:request delegate:self];
+         }
+     }];
+    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"check the server.."];
     [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 }
+
 @end
