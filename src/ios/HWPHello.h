3aca40d72b54170b6fe827b5844eaba63ae26296
selfservit <chak@selfservit.com>
2017-03-20 17:53:31 +0530

    Update HWPHello.h

--- src/ios/HWPHello.h
+++ src/ios/HWPHello.h
@@ -9,12 +9,12 @@
 @interface HWPHello : CDVPlugin <CLLocationManagerDelegate>
 - (void)StartService:(CDVInvokedUrlCommand*)command;
 - (void)SendLocation:(CDVInvokedUrlCommand*)command;
-- (void)getLastKnownLocation:(CDVInvokedUrlCommand*)command;
+- (void)GetLocation:(CDVInvokedUrlCommand*)command;
 - (void)timeReader:(CDVInvokedUrlCommand*)command;
 - (void)CheckSumIndicatorResult:(CDVInvokedUrlCommand*)command;
-- (void)CheckLocationServiceEnabled:(CDVInvokedUrlCommand*)command;
+- (void)CheckLocation:(CDVInvokedUrlCommand*)command;
 - (void)DespatchQueue:(CDVInvokedUrlCommand*)command;
-+(BOOL)CopyFileFromPath:(NSString *)source toDestination:(NSString *)destination;
++(BOOL)CopyFile:(NSString *)source toDestination:(NSString *)destination;
 @property (strong, nonatomic) CLLocationManager * locationManager;
 @property (strong, nonatomic) NSMutableArray * coordinatesArray;
 @end
