55eff7672b88e202fd8e903b726ef7b1bcec0574
selfservit <chak@selfservit.com>
2017-04-12 12:13:42 +0530

    Update HWPHello.h

--- src/ios/HWPHello.h
+++ src/ios/HWPHello.h
@@ -5,17 +5,30 @@
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
 #import <Cordova/CDVPlugin.h>
+#import <UIKit/UIKit.h>
 
-@interface HWPHello : CDVPlugin <CLLocationManagerDelegate>
+@interface HWPHello : CDVPlugin <CLLocationManagerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIAlertViewDelegate>
+{
+    UIImagePickerController *ipc;
+    UIPopoverController *popover;
+}
 - (void)StartService:(CDVInvokedUrlCommand*)command;
 - (void)SendLocation:(CDVInvokedUrlCommand*)command;
-- (void)SendLocation22:(CDVInvokedUrlCommand*)command;
 - (void)GetLocation:(CDVInvokedUrlCommand*)command;
 - (void)timeReader:(CDVInvokedUrlCommand*)command;
 - (void)CheckSumIndicatorResult:(CDVInvokedUrlCommand*)command;
 - (void)CheckLocation:(CDVInvokedUrlCommand*)command;
 - (void)DespatchQueue:(CDVInvokedUrlCommand*)command;
-+(BOOL)CopyFile:(NSString *)source toDestination:(NSString *)destination;
+- (void)CopyFileFromPath:(NSString *)source toDestination:(NSString *)destination;
+- (void)copyFileExample:(CDVInvokedUrlCommand*)command;
+- (void)FileChooser:(CDVInvokedUrlCommand*)command;
+- (NSString *)contentTypeForImageData:(NSData *)data;
+- (void)UpdateChoice:(CDVInvokedUrlCommand*)command;
+- (void)UpdateConfirm:(CDVInvokedUrlCommand*)command;
 @property (strong, nonatomic) CLLocationManager * locationManager;
 @property (strong, nonatomic) NSMutableArray * coordinatesArray;
+@property (weak, nonatomic) IBOutlet UIButton *btnGallery;
+@property (strong, nonatomic) UIViewController * viewcont;
+@property (nonatomic, strong) NSString *callbackIdForImagePicker;
+@property (nonatomic, strong) NSString *callbackIdForAppUpdate;
 @end
