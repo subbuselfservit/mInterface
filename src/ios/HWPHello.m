e12a8c48c0a5150f63ef1094e72c1be6b99f4246
chakm72 <selfservit@gmail.com>
2017-05-10 16:17:01 +0530

    Update HWPHello.m

--- src/ios/HWPHello.m
+++ src/ios/HWPHello.m
@@ -279,59 +279,71 @@
 
 - (void)RefreshTimeProfile:(CDVInvokedUrlCommand*)command
 {
-    [self.commandDelegate runInBackground:^{
-        NSMutableDictionary * dict = [[command arguments] objectAtIndex:0];
-        NSString *date = dict[@"serverDate"];
-        NSString *hour = dict[@"serverHour"];
-        NSString *minute = dict[@"serverMinute"];
-        [self timeValues:date hour:hour minute:minute];
-        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-    }];
+    @try {
+        [self.commandDelegate runInBackground:^{
+            NSMutableDictionary * dict = [[command arguments] objectAtIndex:0];
+            NSString *date = dict[@"serverDate"];
+            NSString *hour = dict[@"serverHour"];
+            NSString *minute = dict[@"serverMinute"];
+            [self timeValues:date hour:hour minute:minute];
+            CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+            [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        }];
+    } @catch (NSException *exception) {
+        NSLog(@"RefreshTimeProfile Exception is : %@", exception.description);
+    }
 }
 
 -(void)timeValues:(NSString *)date hour:(NSString *)hour minute:(NSString *)minute
 {
-    NSString *serverDate = [NSString stringWithFormat:@"%@,%@,%@,00", date, hour, minute];
-    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-    [dateFormatter setDateFormat:@"yyyy,MM,dd,HH,mm,ss"];
-    NSDate *getDate = [dateFormatter dateFromString:serverDate];
-    NSString *deviceDate = [dateFormatter stringFromDate:[NSDate date]];
-    NSString *finalServerDate = [dateFormatter stringFromDate:getDate];
-    NSMutableDictionary * dictionary = [[NSMutableDictionary alloc] init];
-    [dictionary setValue:finalServerDate forKey:@"serverDate"];
-    [dictionary setValue:finalServerDate forKey:@"initServerDate"];
-    [dictionary setValue:deviceDate forKey:@"initDeviceDate"];
-    NSString *docdir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-    NSString *time_profile = [NSString stringWithFormat:@"%@/mservice/time_profile.txt", docdir];
-    NSLog(@"time profile path : %@", time_profile);
-    NSData *fileContents = [NSJSONSerialization dataWithJSONObject:dictionary options:0 error:nil];
-    [fileContents writeToFile:time_profile atomically:true];
+    @try {
+        NSString *serverDate = [NSString stringWithFormat:@"%@,%@,%@,00", date, hour, minute];
+        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+        [dateFormatter setDateFormat:@"yyyy,MM,dd,HH,mm,ss"];
+        NSDate *getDate = [dateFormatter dateFromString:serverDate];
+        NSString *deviceDate = [dateFormatter stringFromDate:[NSDate date]];
+        NSString *finalServerDate = [dateFormatter stringFromDate:getDate];
+        NSMutableDictionary * dictionary = [[NSMutableDictionary alloc] init];
+        [dictionary setValue:finalServerDate forKey:@"serverDate"];
+        [dictionary setValue:finalServerDate forKey:@"initServerDate"];
+        [dictionary setValue:deviceDate forKey:@"initDeviceDate"];
+        NSString *docdir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+        NSString *time_profile = [NSString stringWithFormat:@"%@/mservice/time_profile.txt", docdir];
+        NSLog(@"time profile path : %@", time_profile);
+        NSData *fileContents = [NSJSONSerialization dataWithJSONObject:dictionary options:0 error:nil];
+        [fileContents writeToFile:time_profile atomically:true];
+    } @catch (NSException *exception) {
+        NSLog(@"timeValues Exception is : %@", exception.description);
+    }
 }
 
 - (void)GetNewDate:(CDVInvokedUrlCommand*)command
 {
-    [self.commandDelegate runInBackground:^{
-        NSDate *final = nil;
-        NSString *docdir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
-        NSString *time_profile = [NSString stringWithFormat:@"%@/mservice/time_profile.txt", docdir];
-        NSData *user_data = [NSData dataWithContentsOfFile:time_profile];
-        NSError *jsonError = nil;
-        NSMutableDictionary * dict = [NSJSONSerialization JSONObjectWithData:user_data options:NSJSONReadingMutableContainers error:&jsonError];
-        NSString *serverDate = dict[@"serverDate"];
-        NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
-        [dateFormatter setDateFormat:@"yyyy,MM,dd,HH,mm,ss"];
-        NSDate *deviceDate = [NSDate date];
-        NSDate *getDate = [dateFormatter dateFromString:serverDate];
-        //NSTimeInterval diff = [deviceDate timeIntervalSinceDate:getDate];
-        NSTimeInterval timeDiff = [deviceDate timeIntervalSinceReferenceDate] - [getDate timeIntervalSinceReferenceDate];
-        if(timeDiff > 0){
-            final = [getDate dateByAddingTimeInterval:timeDiff];
-        }
-        NSString *finalServerDate = [dateFormatter stringFromDate:final];
-        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:finalServerDate];
-        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-    }];
+    @try {
+        [self.commandDelegate runInBackground:^{
+            NSDate *final = nil;
+            NSString *docdir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
+            NSString *time_profile = [NSString stringWithFormat:@"%@/mservice/time_profile.txt", docdir];
+            NSData *user_data = [NSData dataWithContentsOfFile:time_profile];
+            NSError *jsonError = nil;
+            NSMutableDictionary * dict = [NSJSONSerialization JSONObjectWithData:user_data options:NSJSONReadingMutableContainers error:&jsonError];
+            NSString *serverDate = dict[@"serverDate"];
+            NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+            [dateFormatter setDateFormat:@"yyyy,MM,dd,HH,mm,ss"];
+            NSDate *deviceDate = [NSDate date];
+            NSDate *getDate = [dateFormatter dateFromString:serverDate];
+            NSTimeInterval diff = [deviceDate timeIntervalSinceDate:getDate];
+            //NSTimeInterval timeDiff = [deviceDate timeIntervalSinceReferenceDate] - [getDate timeIntervalSinceReferenceDate];
+            if(diff > 0){
+                final = [getDate dateByAddingTimeInterval:diff];
+            }
+            NSString *finalServerDate = [dateFormatter stringFromDate:final];
+            CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:finalServerDate];
+            [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        }];
+    } @catch (NSException *exception) {
+        NSLog(@"GetNewDate Exception is : %@", exception.description);
+    }
 }
 
 - (void)CheckLocation:(CDVInvokedUrlCommand*)command
